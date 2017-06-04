7d1ab356c929c4a2be617c6b3e03c663151dd923
Tito Alvarez <augustoalvarez@gmail.com>
2017-06-04 15:32:32 +0200

    Tests for Chrome

--- build/travis/codeception-tests.sh
+++ build/travis/codeception-tests.sh
@@ -14,10 +14,9 @@ chmod a+x $BASE/build/travis/codeception-tests-adjustments.sh
 sudo $BASE/build/travis/codeception-tests-adjustments.sh $USER $(phpenv version-name)
 
 # Google Chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get update
-sudo apt-get install google-chrome-stable
+cd /tmp
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 # Apache setup
 sudo a2enmod rewrite actions fastcgi alias
